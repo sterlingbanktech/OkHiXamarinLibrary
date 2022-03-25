b7ebab2425c5de824e5bd96c5e659b5a3370d378
Akinnagbe <olamidejames007@gmail.com>
2022-03-25 10:14:47 +0100

    completed okhi android integration with sample app

---
+++ Kryo/Kryo.apple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Kryo
+{
+    /// <summary>
+    /// Interface for Kryo
+    /// </summary>
+    public class KryoImplementation : IKryo
+    {
+    }
+}
